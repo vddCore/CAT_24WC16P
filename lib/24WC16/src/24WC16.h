9fd8942662b02a36f2b490b49d6db9455ca505f4
vddCore <vddcore@outlook.com>
2023-01-12 12:06:04 +0100

    add burst write, improve the function names

--- lib/24WC16/src/24WC16.h
+++ lib/24WC16/src/24WC16.h
@@ -4,6 +4,7 @@
 #define E_TWI_NOT_INITIALIZED 5
 #define E_ADDR_OUT_OF_BOUNDS 6
 #define E_TWI_READ_FAILED 7
+#define E_BURST_TOO_LONG 8
 
 #define EEP_24WC16_CHIP_SIZE 2048
 #define EEP_24WC16_PAGE_SIZE 256
@@ -15,8 +16,10 @@ extern "C" {
 void EEP_24WC16_Initialize(void);
 uint8_t EEP_24WC16_GetFailureReason(void);
 
-bool EEP_24WC16_ReadLinear(uint16_t addr, uint8_t* value);
-bool EEP_24WC16_WriteLinear(uint16_t addr, uint8_t value);
+bool EEP_24WC16_ReadByte(uint16_t addr, uint8_t* value);
+
+bool EEP_24WC16_WriteByte(uint16_t addr, uint8_t value);
+bool EEP_24WC16_WriteBurst(uint16_t addr, uint8_t* values, uint8_t len);
 
 #ifdef __cplusplus
 }
